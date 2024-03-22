ab0e5d7deeff06907cbd0cf2452eb1da2264482e
meilaleinalainengithub <melkerXsievertsson@gmail.com>
2024-03-22 17:14:55 +0100

    did some trolling

--- src/classes.py
+++ src/classes.py
@@ -2,130 +2,158 @@ import random
 
 class Survivors():
     def __init__(self):
-        self.All = ["Dark Sense", "Deja Vu", "Hope", "Kindred", "Lightweight", "No One Left Behind", "Plunderer's Instinct", "Premonition", "Resilience", "Slippery Meat", "Small Game", "Spine Chill", "This Is Not Happening", "We'll Make It"]
-        Ada = ["Low Profile", "Reactive Healing", "Wiretap"]
-        Adam = ["Autodidact", "Deliverance", "Diversion"]
-        Alan = ["Boon: Illumination", "Champion of Light", "Deadline"]
-        Ace = ["Ace in the Hole", "Open-Handed", "Up the Ante"]
-        Ashley = ["Buckle Up", "Flip-Flop", "Mettle of Man"]
-        Cheryl = ["Blood Pact", "Repressed Alliance", "Soul Guard"]
-        Claudette = ["Botany Knowledge", "Empathy", "Self-Care"]
-        Dwight = ["Bond", "Prove Thyself", "Leader"]
-        Ellen = ["Chemical Trap", "Light-Footed", "Lucky Star"]
-        Felix = ["Built to Last", "Desperate Measures", "Visionary"]
-        Gabriel = ["Made for This", "Scavenger", "Troubleshooter"]
-        Haddie = ["Inner Focus", "Overzealous", "Residual Manifest"]
-        Jake = ["Calm Spirit", "Iron Will", "Saboteur"]
-        Jeff = ["Aftercare", "Breakdown", "Distortion"]
-        Jane = ["Head On", "Poised", "Solidarity"]
-        Jill = ["Blast Mine", "Counterforce", "Resurgence"]
-        Jonah = ["Boon: Exponential", "Corrective Action", "Overcome"]
-        Kate = ["Boil Over", "Dance With Me", "Windows of Opportunity"]
-        Laurie = ["Decisive Strike", "Object of Obsession", "Sole Survivor"]
-        Leon = ["Bite the Bullet", "Flashbang", "Flashbang"]
-        Meg = ["Adrenaline", "Quick & Quiet", "Sprint Burst"]
-        Mikaela = ["Boon: Circle of Healing", "Boon: Shadow Step", "Clairvoyance"]
-        Nancy = ["Better Together", "Fixated", "Inner Strength"]
-        Nea = ["Balanced Landing", "Streetwise", "Urban Evasion"]
-        Nicolas = ["Dramaturgy", "Plot Twist", "Scene Partner"]
-        Quentin = ["Pharmacy", "Vigil", "Wake Up!"]
-        Rebecca = ["Better than New", "Hyperfocus", "Reassurance"]
-        Renato = ["Background Player", "Blood Rush", "Teamwork: Collective Stealth"]
-        Sable = ["Invocation: Weaving Spiders", "Strength in Shadows", "Wicked"]
-        Steve = ["Babysitter", "Camaraderie", "Second Wind"]
-        Tapp = ["Detective's Hunch", "Stake Out", "Tenacity"]
-        Thalita = ["Cut Loose", "Friendly Competition", "Teamwork: Power of Two"]
-        Vittorio = ["Fogwise", "Potential Energy", "Quick Gambit"]
-        Bill = ["Borrowed Time", "Left Behind", "Unbreakable"]
-        Yoichi = ["Boon: Dark Theory", "Empathic Connection", "Parental Guidance"]
-        Yui = ["Any Means Necessary", "Breakout", "Lucky Break"]
-        Yun = ["Fast Track", "Self-Preservation", "Smash Hit"]
-        Zarina = ["For the People", "Off the Record", "Red Herring"]
-        Élodie = ["Appraisal", "Deception", "Power Struggle"]
-        David = ["Dead Hard", "No Mither", "We're Gonna Live Forever"]
-        Feng = ["Alert", "Lithe", "Technician"]
-
-        self.survivors = [Dwight, Meg, Claudette, Jake, Nea, Laurie, Ace, Bill, Feng, David, Kate, Quentin, 
-                          Tapp, Adam, Jeff, Jane, Ashley, Nancy, Steve, Yui, Zarina, Cheryl, Felix, Élodie, 
-                          Yun, Jill, Leon, Mikaela, Jonah, Yoichi, Haddie, Ada, Rebecca, Vittorio, Thalita,
-                          Renato, Gabriel, Nicolas, Ellen, Alan, Sable]
+        self.survivors = {
+            "Ada": ["Low Profile", "Reactive Healing", "Wiretap"],
+            "Adam": ["Autodidact", "Deliverance", "Diversion"],
+            "Alan": ["Boon: Illumination", "Champion of Light", "Deadline"],
+            "Ace": ["Ace in the Hole", "Open-Handed", "Up the Ante"],
+            "Ashley": ["Buckle Up", "Flip-Flop", "Mettle of Man"],
+            "Cheryl": ["Blood Pact", "Repressed Alliance", "Soul Guard"],
+            "Claudette": ["Botany Knowledge", "Empathy", "Self-Care"],
+            "Dwight": ["Bond", "Prove Thyself", "Leader"],
+            "Ellen": ["Chemical Trap", "Light-Footed", "Lucky Star"],
+            "Felix": ["Built to Last", "Desperate Measures", "Visionary"],
+            "Gabriel": ["Made for This", "Scavenger", "Troubleshooter"],
+            "Haddie": ["Inner Focus", "Overzealous", "Residual Manifest"],
+            "Jake": ["Calm Spirit", "Iron Will", "Saboteur"],
+            "Jeff": ["Aftercare", "Breakdown", "Distortion"],
+            "Jane": ["Head On", "Poised", "Solidarity"],
+            "Jill": ["Blast Mine", "Counterforce", "Resurgence"],
+            "Jonah": ["Boon: Exponential", "Corrective Action", "Overcome"],
+            "Kate": ["Boil Over", "Dance With Me", "Windows of Opportunity"],
+            "Laurie": ["Decisive Strike", "Object of Obsession", "Sole Survivor"],
+            "Leon": ["Bite the Bullet", "Flashbang", "Flashbang"],
+            "Meg": ["Adrenaline", "Quick & Quiet", "Sprint Burst"],
+            "Mikaela": ["Boon: Circle of Healing", "Boon: Shadow Step", "Clairvoyance"],
+            "Nancy": ["Better Together", "Fixated", "Inner Strength"],
+            "Nea": ["Balanced Landing", "Streetwise", "Urban Evasion"],
+            "Nicolas": ["Dramaturgy", "Plot Twist", "Scene Partner"],
+            "Quentin": ["Pharmacy", "Vigil", "Wake Up!"],
+            "Rebecca": ["Better than New", "Hyperfocus", "Reassurance"],
+            "Renato": ["Background Player", "Blood Rush", "Teamwork: Collective Stealth"],
+            "Sable": ["Invocation: Weaving Spiders", "Strength in Shadows", "Wicked"],
+            "Steve": ["Babysitter", "Camaraderie", "Second Wind"],
+            "Tapp": ["Detective's Hunch", "Stake Out", "Tenacity"],
+            "Thalita": ["Cut Loose", "Friendly Competition", "Teamwork: Power of Two"],
+            "Vittorio": ["Fogwise", "Potential Energy", "Quick Gambit"],
+            "Bill": ["Borrowed Time", "Left Behind", "Unbreakable"],
+            "Yoichi": ["Boon: Dark Theory", "Empathic Connection", "Parental Guidance"],
+            "Yui": ["Any Means Necessary", "Breakout", "Lucky Break"],
+            "Yun": ["Fast Track", "Self-Preservation", "Smash Hit"],
+            "Zarina": ["For the People", "Off the Record", "Red Herring"],
+            "Élodie": ["Appraisal", "Deception", "Power Struggle"],
+            "David": ["Dead Hard", "No Mither", "We're Gonna Live Forever"],
+            "Feng": ["Alert", "Lithe", "Technician"],
+            "All": ["Dark Sense", "Deja Vu", "Hope", "Kindred", "Lightweight", "No One Left Behind", "Plunderer's Instinct", "Premonition", "Resilience", "Slippery Meat", "Small Game", "Spine Chill", "This Is Not Happening", "We'll Make It"]
+        }
         
-    def randomizePerk(self, amount: int, exclude: list = None):
-        if exclude:
-            chosen = []    
-            self.tempSurvivors = self.survivors
+    def randomizePerk(self, amount: int, exclude: str = None):  
+        chosen = []
+        self.tempSurvivors = self.survivors
+        if exclude:    
             for survivor in exclude:
                 self.tempSurvivors.remove(survivor)
     
-        for i in range(amount):
-            character = random.randrange(self.survivors)
-            chosen.append(random.randrange(character))
+        for _ in range(int(amount)):
+            character, perks = random.choice(list(self.tempSurvivors.items()))
+            chosen_perk = random.choice(perks)
+            chosen.append((character, chosen_perk))
 
         return chosen
     
-    def randomizeSurvivor(self, exclude: list = None):
+    def randomizeSurvivor(self, exclude: str = None):   
+        self.tempSurvivors = self.survivors
         if exclude:  
-            self.tempSurvivors = self.survivors
             for survivor in exclude:
                 self.tempSurvivors.remove(survivor)
-            return random.randrange(self.survivors)
+        return random.choice(self.tempSurvivors)
         
 class Killers():
     def __init__(self):
-        Artist = []
-        Blight = []
-        Cannibal = []
-        Cenobite = []
-        Clown = []
-        Deathslinger = []
-        Demogorgon = []
-        Doctor = []
-        Executioner = []
-        Ghost_Face = []
-        Good_Guy = []
-        Hag = []
-        Huntress = []
-        Knight = []
-        Legion = []
-        Mastermind = []
-        Nightmare = []
-        Nurse = []
-        Oni = []
-        Pig = []
-        Plague = []
-        Skull_Merchant = []
-        Spirit = []
-        Trapper = []
-        Twins = []
-        Trickster = []
-        Unknown = []
-        Wraith = []
-        Xenomorph = []
-        Onryō = []
-        Dredge = []
-        Nemesis = []
-
-        self.killers = [Artist, Blight, Cannibal, Cenobite, Clown, Deathslinger, Demogorgon, Doctor, Executioner,
-                        Ghost_Face, Good_Guy, Hag, Huntress, Knight, Legion, Mastermind, Nightmare, Nurse, Oni, Pig,
-                        Plague, Skull_Merchant, Spirit, Trapper, Twins, Trickster, Unknown, Wraith, Xenomorph, Onryō,
-                        Dredge, Nemesis]
+        self.killers = {
+            "Artist": [],
+            "Blight": [],
+            "Cannibal": [],
+            "Cenobite": [],
+            "Clown": [],
+            "Deathslinger": [],
+            "Demogorgon": [],
+            "Doctor": [],
+            "Executioner": [],
+            "Ghost_Face": [],
+            "Good_Guy": [],
+            "Hag": [],
+            "Huntress": [],
+            "Knight": [],
+            "Legion": [],
+            "Mastermind": [],
+            "Nightmare": [],
+            "Nurse": [],
+            "Oni": [],
+            "Pig": [],
+            "Plague": [],
+            "Skull_Merchant": [],
+            "Spirit": [],
+            "Trapper": [],
+            "Twins": [],
+            "Trickster": [],
+            "Unknown": [],
+            "Wraith": [],
+            "Xenomorph": [],
+            "Onryō": [],
+            "Dredge": [],
+            "Nemesis": [],
+            "Artist": [],
+            "Blight": [],
+            "Cannibal": [],
+            "Cenobite": [],
+            "Clown": [],
+            "Deathslinger": [],
+            "Demogorgon": [],
+            "Doctor": [],
+            "Executioner": [],
+            "Ghost_Face": [],
+            "Good_Guy": [],
+            "Hag": [],
+            "Huntress": [],
+            "Knight": [],
+            "Legion": [],
+            "Mastermind": [],
+            "Nightmare": [],
+            "Nurse": [],
+            "Oni": [],
+            "Pig": [],
+            "Plague": [],
+            "Skull_Merchant": [],
+            "Spirit": [],
+            "Trapper": [],
+            "Twins": [],
+            "Trickster": [],
+            "Unknown": [],
+            "Wraith": [],
+            "Xenomorph": [],
+            "Onryō": [],
+            "Dredge": [],
+            "Nemesis": [],
+        }
         
-    def randomizePerk(self, amount: int, exclude: list = None):
+    def randomizePerk(self, amount: int, exclude: str = None):  
         chosen = []
+        self.tempKiller = self.killers
         if exclude:    
-            self.tempkillers = self.killers
-            for killers in exclude:
-                self.tempKiller.remove(killers)
+            for killer in exclude:
+                self.tempKiller.remove(killer)
     
-        for i in range(amount):
-            character = random.randrange(self.killers)
-            chosen.append(random.randrange(character))
+        for _ in range(int(amount)):
+            character, perks = random.choice(list(self.killers.items()))
+            chosen_perk = random.choice(perks)
+            chosen.append((character, chosen_perk))
 
         return chosen
     
-    def randomizeKiller(self, exclude: list = None):
+    def randomizeKiller(self, exclude: str = None):
         if exclude:  
             self.tempKiller = self.killers
             for killers in exclude:
                 self.tempKiller.remove(killers)
-            return random.randrange(self.killers)
+            return random.choice(self.killers)
--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
 import requests, discord, os, time, datetime, classes
 from dotenv import load_dotenv
-from discord import app_commands
 from discord.ext import commands
+from discord import app_commands
 load_dotenv('.env')
 
 # FUCKED UP SHIT:
-# - NON- SLASH COMMANDS
-# - SLASH COMMANDS
-# - I HAVE 2 CLIENTS
+# - PARAMETERS ARE FUCKED
 # - MATH
 
 Survivors = classes.Survivors()
 Killers = classes.Killers()
 
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix=';', intents=intents, client=client)
-cmds = app_commands.CommandTree(client=client)
+bot = commands.Bot(command_prefix=';', intents=discord.Intents.all())
 
 def format_time_difference(time_difference):
     days = time_difference.days
@@ -24,13 +19,13 @@ def format_time_difference(time_difference):
     minutes, seconds = divmod(remainder, 60)
     return f"{days} Days, {hours} Hours, {minutes} Minutes, {seconds} Seconds"
 
-@client.event
+@bot.event
 async def on_ready():
-    await cmds.sync(guild=discord.Object(id=1198070162432200865))
-    print("ready")
+    e = await bot.tree.sync()
+    print(e)
 
-@bot.command(name="helpme")
-async def helpme(ctx):
+@bot.tree.command(name="help", description="Helps You... read the name")
+async def helpme(interaction: discord.Interaction):
     embed = discord.Embed(
         title="Dead By Daylight Tracker",
         description='''**Commands**\n
@@ -39,10 +34,10 @@ async def helpme(ctx):
         color=discord.Colour.dark_grey()
     )
 
-    await ctx.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
-@bot.command(name="shrine")
-async def shrine(ctx):
+@bot.tree.command(name="shrine", description="Shows current Shrine of Secrets")
+async def shrine(interaction: discord.Interaction):
     response = requests.get("https://dbd.tricky.lol/api/shrine")
     if response.status_code == 200:
         data = response.json()
@@ -56,12 +51,12 @@ async def shrine(ctx):
         embed.add_field(name="Ends: ", value=datetime.datetime.fromtimestamp(data['end']).strftime('%Y-%m-%d %H:%M:%S'), inline=True)
         embed.add_field(name="Perks:", value="\n\n".join([f"**{perk['id']}**\n**Bloodpoints:** {perk['bloodpoints']},\n**Shards:** {perk['shards']}" for perk in data['perks']]), inline=False)
 
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
     else:
         print("IT DIDNT WORK FUCKER")
 
-@bot.command(name="reset")
-async def reset(ctx):
+@bot.tree.command(name="reset", description="Shows next and past rank reset")
+async def reset(interaction: discord.Interaction):
     current_time = datetime.datetime.now()
     last_target_date = datetime.datetime(current_time.year, current_time.month, 13)
     last_time_difference = current_time - last_target_date
@@ -84,23 +79,23 @@ async def reset(ctx):
     embed.add_field(name="**Last Reset:**", value=f"{last_reset} ago", inline=False)
     embed.add_field(name="**Next Reset:**", value=f"CURRENTLY FUCKED CUS I CANT TO MATH", inline=False)
 
-    await ctx.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
-@bot.command(name="randomperk")
-async def randomperk(ctx, role: str, amount: str, exclude: list = None):
+@bot.tree.command(name="randomperk", description="idk man gives you a random character")
+async def randomperk(interaction: discord.Interaction, role: str, amount: int, exclude: str = None):
     if role.lower() == "survivor":
         embedSurvior = discord.Embed(
             title=f"Random Perks, Survivor - {time.strftime('%m/%d/%H:%M:%S')}",
             color=discord.Color.dark_grey())
 
-        if int(amount) > 4 or int(amount) < 0:
-            await ctx.send("Please select an amount within 1-4.")
+        if int(amount) > 4 or int(amount) < 0:  
+            await interaction.response.send_message("Please select an amount within 1-4.")
 
-        perks = Survivors.randomizePerk(amount=int(amount), exclude=exclude)
+        perks, character = Survivors.randomizePerk(amount=int(amount), exclude=exclude)
         for i in range(int(amount)):
-            embedSurvior.add_field(name=f"Perk{i}: ", value=perks[i-1])
+            embedSurvior.add_field(name=f"Perk {i+1}: ", value=f'{perks[i-1]} - {character}', inline=False)
 
-        await ctx.send(embed=embedSurvior)
+        await interaction.response.send_message(embed=embedSurvior)
 
     elif role.lower() == "killer":
         embedKiller = discord.Embed(
@@ -108,19 +103,19 @@ async def randomperk(ctx, role: str, amount: str, exclude: list = None):
             color=discord.Color.dark_grey())
         
         if int(amount) > 4 or int(amount) < 0:
-            await ctx.send("Please select an amount within 1-4.")
+            await interaction.response.send_message("Please select an amount within 1-4.")
 
-        perks = Killers.randomizePerk(amount=int(amount), exclude=exclude)
+        perks, character = Killers.randomizePerk(amount=int(amount), exclude=exclude)
         for i in range(int(amount)):
-            embedKiller.add_field(name=f"Perk{i}: ", value=perks[i-1])
+            embedKiller.add_field(name=f"Perk {i+1}: ", value=f'{perks[i-1]} - {character}', inline=False)
 
-        await ctx.send(embed=embedKiller)
+        await interaction.response.send_message(embed=embedKiller)
 
     else:
-        await ctx.send("Invalid Role. Please select 'killer' or 'survivor'.")
+        await interaction.response.send_message("Invalid Role. Please select 'killer' or 'survivor'.")
 
-@bot.command(name="randomcharacter")
-async def randomcharacter(ctx, role: str, exclude: list = None):
+@bot.tree.command(name="randomcharacter", description="idk man gives you a random character")
+async def randomcharacter(interaction: discord.Interaction, role: str, exclude: str = None):
     if role.lower() == "survivor":
         embedSurvior = discord.Embed(
             title=f"Random Survivor - {time.strftime('%m/%d/%H:%M:%S')}",
@@ -128,7 +123,7 @@ async def randomcharacter(ctx, role: str, exclude: list = None):
         
         survivor = Survivors.randomizeSurvivor(exclude=exclude)
         embedSurvior.add_field(name="Survivor:", value=survivor, inline=False)
-        await ctx.send(embed=embedSurvior)
+        await interaction.response.send_message(embed=embedSurvior)
 
     elif role.lower() == "killer":
         embedKiller = discord.Embed(
@@ -137,13 +132,9 @@ async def randomcharacter(ctx, role: str, exclude: list = None):
 
         killer = Killers.randomizeKiller(exclude=exclude)
         embedKiller.add_field(name="Killer:", value=killer, inline=False)
-        await ctx.send(embed=embedKiller)
+        await interaction.response.send_message(embed=embedKiller)
 
     else:
-        await ctx.send("Please enter a valid role. Survivor/Killer")
+        await interaction.response.send_message("Please enter a valid role. Survivor/Killer")
 
-@cmds.command(name="test", description="description") 
-async def test(interaction: discord.Interaction):    
-    await interaction.response.send_message("command")
-
-client.run(os.getenv('DISCORD_TOKEN'))
+bot.run(os.getenv('DISCORD_TOKEN'))
